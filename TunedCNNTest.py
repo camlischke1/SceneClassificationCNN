47bc56f29e17a98c930e77ef46bee06e0f33efe0
Cam Lischke <61704172+camlischke1@users.noreply.github.com>
2020-11-30 14:20:39 -0500

    Add files via upload

--- TunedCNNTest.py
+++ TunedCNNTest.py
@@ -11,25 +11,28 @@ from scipy import signal
 
 
 X_test = np.load("X_test.npy")
-model = load_model("tuned_model.keras")
+model = load_model("scene_classification_transfer_learning_1.keras")
 
-#plt.imshow(X_test[1])
-#plt.show()
-prediction = model.predict(X_test)
-prediction = np.argmax(prediction,axis=1)
+dict = {0:"Buildings",1:"Forest",2:"Glacier",3:"Mountains",4:"Sea",5:"Street"}
 
 width=10
 height=10
 rows = 3
 cols = 3
+off =40
 axes=[]
 fig=plt.figure()
+prediction = model.predict(X_test[off:off+rows*cols])
+prediction = np.argmax(prediction,axis=1)
 
-for a in range(rows*cols):
+
+
+for a in range(off,off+rows*cols):
     b = X_test[a]
-    axes.append( fig.add_subplot(rows, cols, a+1) )
-    subplot_title=("Predicted"+ str(prediction[a]))
+    axes.append( fig.add_subplot(rows, cols, a+1-off) )
+    subplot_title=(str(dict.get(prediction[a-off])))
     axes[-1].set_title(subplot_title)
+    plt.axis('off')
     plt.imshow(b)
 fig.tight_layout()
 plt.show()
